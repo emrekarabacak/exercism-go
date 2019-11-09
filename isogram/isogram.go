ae45b096251be9cee6512020672b0534f353d70e
Emre Karabacak <emrekarabacak@outlook.com>
2019-11-10 01:01:33 +0300

    isogram solution added

---
+++ isogram/isogram.go
@@ -0,0 +1,18 @@
+package isogram
+
+import (
+	"strings"
+	"unicode"
+)
+
+// IsIsogram determines a given word is isogram or not
+func IsIsogram(word string) bool {
+	letters := make(map[rune]bool)
+	for _, ch := range strings.ToLower(word) {
+		if unicode.IsLetter(ch) && letters[ch] != false {
+			return false
+		}
+		letters[ch] = true
+	}
+	return true
+}
