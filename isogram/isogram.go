8d8691d454079d2ab4443732d475891d4a132774
Emre Karabacak <emrekarabacak@outlook.com>
2019-11-10 01:07:35 +0300

    isogram updated

--- isogram/isogram.go
+++ isogram/isogram.go
@@ -7,7 +7,7 @@ import (
 
 // IsIsogram determines a given word is isogram or not
 func IsIsogram(word string) bool {
-	letters := make(map[rune]bool)
+	letters := make(map[rune]bool, len(word))
 	for _, ch := range strings.ToLower(word) {
 		if unicode.IsLetter(ch) && letters[ch] != false {
 			return false
